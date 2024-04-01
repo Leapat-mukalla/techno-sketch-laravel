195cab0ea656ee553168a067f4814ffdb4b2a750
smaalfadly3 <smaalfadly@gmail.com>
2024-04-01 22:20:00 +0300

    add video attributes

--- resources/views/reception-home.blade.php
+++ resources/views/reception-home.blade.php
@@ -126,7 +126,7 @@
                     </div>
                     <div class="col-12">
                         <div class=" d-flex flex-column align-items-center justify-content-center" style=" ">
-                            <video class="w-100  " id="camera" autoplay style="  height: 55vh; object-fit: cover;"></video>
+                            <video class="w-100  " id="camera" autoplay playsinline muted style="  height: 55vh; object-fit: cover;"></video>
                             <button class=" btn btn-primary mt-2" id="captureButton">مسح</button>
                         </div>
                     </div>
